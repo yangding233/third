6f2ffa4c2e3ba58bddeb19f3ad0b9a7c22c3b77b
yangding233 <150337404+yangding233@users.noreply.github.com>
2024-03-10 00:31:43 +0800

    Add files via upload

---
+++ third/third_class.c
@@ -0,0 +1,69 @@
+//==================if and else比大小运用=====================
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(){
+//     int num1;
+//     int num2;
+//     printf("please enter your number to num1 and num2\n");
+//     scanf("%d",&num1);
+//     scanf("%d",&num2);
+//     if (num1>=num2)
+//         printf("the max number is %d\n",num1);
+//     else
+//         printf("the max number is %d\n",num2);
+//     system("pause");
+//     return 0;
+//     }
+//===========比大小===========================================
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(){
+//     int a;
+//     int b;
+//     printf("please write two\n");
+//     scanf("%d,%d",a,b);
+//     a>b?printf("the max is a"): printf("the max is b"); 
+//     system("pause");
+//     return 0;
+//     }
+//============"~对一个数的二进制按位取反"听不懂没关系，之后会讲=====================
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(){
+//     int a = 0;
+//     int b = ~a;
+//     printf("%d\n", b);
+//     system("pause");
+//     return 0;
+//     }
+//====================================================
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(){
+//     int a = 10;
+//     //int b = a++;//后置++：先使用，再++//--同理
+//     int c = ++a;//前置++：先++，后使用//--同理
+//     printf("%d\n", a);
+//     //printf("%d\n", b);
+//     printf("%d\n", c);
+//     system("pause");
+//     return 0;
+//     }
+//==================秒数转换=================================
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(){
+//     int a;
+//     int h;
+//     int min;
+//     int s;
+//     printf("please enter second your want!\n");
+//     scanf("%d",&a);
+//     h = a/3600;
+//     min = (a%3600)/60;
+//     s = a%60;
+//     printf("the end is h=%d min=%d s=%d!\n",h,min,s);
+//     system("pause");
+//     return 0;
+//     }
+
